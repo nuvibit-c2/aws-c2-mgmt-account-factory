b9d3c1d65812b0220074dfbb5b998326f8ead58f
stefano-franco <stefano.franco@nuvibit.com>
2023-06-03 23:16:00 +0200

    revert migration

--- ntc_parameters.tf
+++ ntc_parameters.tf
@@ -13,6 +13,49 @@ locals {
 
   # by default existing node parameters will be merged with new parameters to avoid deleting parameters
   ntc_parameters_replace = true
+
+  # the ntc parameter bucket should ideally be created in same pipeline as account factory
+  # all organization accounts are granted read permission for all parameters
+  # only the parameter node owner account is granted write access to his corresponding parameters
+  ntc_parameter_nodes = [
+    {
+      "node_name"                = "account-factory",
+      "node_owner_account_id"    = local.account_factory_core_account_ids["aws-c2-management"]
+      "node_owner_iam_user_name" = "aws-c2-account-factory"
+    },
+    {
+      "node_name"                = "management",
+      "node_owner_account_id"    = local.account_factory_core_account_ids["aws-c2-management"]
+      "node_owner_iam_user_name" = "aws-c2-management"
+    },
+    {
+      "node_name"             = "connectivity"
+      "node_owner_account_id" = local.account_factory_core_account_ids["aws-c2-connectivity"]
+    },
+    {
+      "node_name"             = "security"
+      "node_owner_account_id" = local.account_factory_core_account_ids["aws-c2-security"]
+    },
+    {
+      "node_name"             = "log-archive"
+      "node_owner_account_id" = local.account_factory_core_account_ids["aws-c2-log-archive"]
+    }
+  ]
+
+  # parameters are shared with parameter node owners by default and with the entire organization if org_id is specified
+  share_parameters_with_entire_org = false
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ NTC PARAMETERS - BUCKET (DEPLOY FIRST)
+# ---------------------------------------------------------------------------------------------------------------------
+module "ntc_parameters_bucket" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters?ref=beta"
+
+  force_destroy   = false
+  bucket_name     = local.ntc_parameters_bucket_name
+  org_id          = local.share_parameters_with_entire_org ? data.aws_organizations_organization.current.id : ""
+  parameter_nodes = local.ntc_parameter_nodes
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
