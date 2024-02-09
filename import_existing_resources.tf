4d81dbfb08497b75d7c2e8e076269f7f38f09ef3
stefano-franco <stefano.franco@nuvibit.com>
2024-02-09 09:03:05 +0100

    import more

--- import_existing_resources.tf
+++ import_existing_resources.tf
@@ -10,66 +10,50 @@ import {
 */
 
 import {
-  to = module.account_factory.aws_organizations_account.ntc_factory_account["aws-c2-log-archive"]
-  id = "872327204802"
+  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_role["security-core"]
+  id = "ntc-af-pipeline-role__security-core"
 }
 import {
-  to = module.account_factory.aws_organizations_account.ntc_factory_account["aws-c2-security"]
-  id = "769269768678"
+  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_role["global"]
+  id = "ntc-af-pipeline-role__global"
 }
 import {
-  to = module.account_factory.aws_organizations_account.ntc_factory_account["aws-c2-0002"]
-  id = "090258021222"
+  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_role["workloads-prod"]
+  id = "ntc-af-pipeline-role__workloads-prod"
 }
 import {
-  to = module.account_factory.aws_organizations_account.ntc_factory_account["aws-c2-management"]
-  id = "228120440352"
+  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_event_rule_role
+  id = "ntc-af-pipeline-trigger-role"
 }
 import {
-  to = module.account_factory.aws_organizations_account.ntc_factory_account["aws-c2-playground-test"]
-  id = "143858593822"
+  to = module.account_factory.module.account_factory_cloudtrail.aws_cloudtrail.ntc_org_events_trail
+  id = "ntc-af-org-events-trail"
 }
 import {
-  to = module.account_factory.aws_organizations_account.ntc_factory_account["aws-c2-connectivity"]
-  id = "944538260333"
+  to = module.account_factory.module.lambda_pretty_notifications.aws_lambda_function.ntc_lambda
+  id = "ntc-af-notification-lambda"
 }
 import {
-  to = module.account_factory.aws_organizations_account.ntc_factory_account["aws-c2-0001"]
-  id = "945766593056"
+  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_invite_security_members"].aws_lambda_function.ntc_lambda
+  id = "on_CreateAccountResult_invite_security_members"
 }
 import {
-  to = module.account_factory.aws_iam_role.ntc_lambda_execution_role
-  id = "ntc-af-lambda-execution-role"
+  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_enable_opt_in_regions"].aws_lambda_function.ntc_lambda
+  id = "on_CreateAccountResult_enable_opt_in_regions"
 }
 import {
-  to = module.account_factory.aws_kms_alias.ntc_account_factory_encryption
-  id = "alias/ntc-af-encryption"
+  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_tag_shared_resources"].aws_lambda_function.ntc_lambda
+  id = "on_CreateAccountResult_tag_shared_resources"
 }
 import {
-  to = module.account_factory.aws_iam_role.ntc_state_machine_event_rule_role
-  id = "ntc-af-event-forwarding-role-trigger-role"
+  to = module.account_factory.module.lambda_step_function["on_CloseAccountResult_move_to_suspended_ou"].aws_lambda_function.ntc_lambda
+  id = "on_CloseAccountResult_move_to_suspended_ou"
 }
 import {
-  to = module.ntc_parameters_bucket.aws_s3_bucket.ntc_parameters
-  id = "aws-c2-ntc-parameters"
+  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_increase_service_quota"].aws_lambda_function.ntc_lambda
+  id = "on_CreateAccountResult_increase_service_quota"
 }
 import {
-  to = module.account_factory.module.baseline_artifacts_bucket.aws_s3_bucket.ntc_bucket
-  id = "aws-c2-ntc-af-baseline-artifacts"
-}
-import {
-  to = module.account_factory.module.baseline_bucket.aws_s3_bucket.ntc_bucket
-  id = "aws-c2-ntc-af-baseline"
-}
-import {
-  to = module.account_factory.module.account_factory_cloudtrail.module.cloudtrail_bucket.aws_s3_bucket.ntc_bucket
-  id = "aws-c2-ntc-af-cloudtrail"
-}
-import {
-  to = module.account_factory.module.account_factory_cloudtrail.aws_kms_alias.ntc_cloudtrail_encryption
-  id = "alias/ntc-af-cloudtrail-encryption"
-}
-import {
-  to = module.account_factory.module.account_factory_cloudtrail.aws_iam_role.ntc_event_bus_forward[0]
-  id = "ntc-af-event-forwarding-role"
+  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_delete_default_vpc"].aws_lambda_function.ntc_lambda
+  id = "on_CreateAccountResult_delete_default_vpc"
 }
