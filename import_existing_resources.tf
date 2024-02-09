e844e62e50bb27eec347f9b3f92fe52a30bd9d01
stefano-franco <stefano.franco@nuvibit.com>
2024-02-09 09:18:28 +0100

    import more

--- import_existing_resources.tf
+++ import_existing_resources.tf
@@ -10,50 +10,14 @@ import {
 */
 
 import {
-  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_role["security-core"]
-  id = "ntc-af-pipeline-role__security-core"
+  to = module.account_factory.aws_codebuild_project.ntc_baseline_project["security-core"]
+  id = "ntc-af__security-core"
 }
 import {
-  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_role["global"]
-  id = "ntc-af-pipeline-role__global"
+  to = module.account_factory.aws_codebuild_project.ntc_baseline_project["global"]
+  id = "ntc-af__global"
 }
 import {
-  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_role["workloads-prod"]
-  id = "ntc-af-pipeline-role__workloads-prod"
-}
-import {
-  to = module.account_factory.aws_iam_role.ntc_baseline_pipeline_event_rule_role
-  id = "ntc-af-pipeline-trigger-role"
-}
-import {
-  to = module.account_factory.module.account_factory_cloudtrail.aws_cloudtrail.ntc_org_events_trail
-  id = "ntc-af-org-events-trail"
-}
-import {
-  to = module.account_factory.module.lambda_pretty_notifications.aws_lambda_function.ntc_lambda
-  id = "ntc-af-notification-lambda"
-}
-import {
-  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_invite_security_members"].aws_lambda_function.ntc_lambda
-  id = "on_CreateAccountResult_invite_security_members"
-}
-import {
-  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_enable_opt_in_regions"].aws_lambda_function.ntc_lambda
-  id = "on_CreateAccountResult_enable_opt_in_regions"
-}
-import {
-  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_tag_shared_resources"].aws_lambda_function.ntc_lambda
-  id = "on_CreateAccountResult_tag_shared_resources"
-}
-import {
-  to = module.account_factory.module.lambda_step_function["on_CloseAccountResult_move_to_suspended_ou"].aws_lambda_function.ntc_lambda
-  id = "on_CloseAccountResult_move_to_suspended_ou"
-}
-import {
-  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_increase_service_quota"].aws_lambda_function.ntc_lambda
-  id = "on_CreateAccountResult_increase_service_quota"
-}
-import {
-  to = module.account_factory.module.lambda_step_function["on_CreateAccountResult_delete_default_vpc"].aws_lambda_function.ntc_lambda
-  id = "on_CreateAccountResult_delete_default_vpc"
+  to = module.account_factory.aws_codebuild_project.ntc_baseline_project["workloads-prod"]
+  id = "ntc-af__workloads-prod"
 }
