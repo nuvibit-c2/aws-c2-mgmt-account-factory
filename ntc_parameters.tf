b403a5c63090029c2fd6f83c8eece2b1bdbac94a
stefano-franco <stefano.franco@nuvibit.com>
2024-02-09 21:22:27 +0100

    fix reference

--- ntc_parameters.tf
+++ ntc_parameters.tf
@@ -20,98 +20,7 @@ locals {
   share_parameters_with_entire_org = false
 
   # map of parameters merged from all parameter nodes
-  ntc_parameters = {
-    "mgmt-account-factory": {
-      "core_accounts": {
-        "aws-c2-connectivity": "944538260333",
-        "aws-c2-log-archive": "872327204802",
-        "aws-c2-management": "228120440352",
-        "aws-c2-security": "769269768678"
-      }
-    },
-    "connectivity": {
-      "customer_managed_prefix_lists": {
-        "onprem-ipv4-ranges": {
-          "address_family": "IPv4",
-          "arn": "arn:aws:ec2:eu-central-1:944538260333:prefix-list/pl-055dcaa9a551fe6ad",
-          "entries": [
-            {
-              "cidr": "192.168.10.0/24",
-              "description": "Server Zone A"
-            },
-            {
-              "cidr": "192.168.20.0/24",
-              "description": "Server Zone B"
-            },
-            {
-              "cidr": "192.168.30.0/24",
-              "description": "Server Zone C"
-            }
-          ],
-          "id": "pl-055dcaa9a551fe6ad",
-          "max_entries": 3,
-          "name": "onprem-ipv4-ranges",
-          "version": 4
-        }
-      }
-    },
-    "mgmt-identity-center": {},
-    "log-archive": {
-      "log_bucket_arns": {
-        "aws_config": "arn:aws:s3:::aws-c2-config-archive",
-        "dns_query_logs": "arn:aws:s3:::aws-c2-dns-query-logs-archive",
-        "guardduty": "arn:aws:s3:::aws-c2-guardduty-archive",
-        "org_cloudtrail": "arn:aws:s3:::aws-c2-cloudtrail-archive",
-        "s3_access_logging": "arn:aws:s3:::aws-c2-access-logging",
-        "vpc_flow_logs": "arn:aws:s3:::aws-c2-vpc-flow-logs-archive"
-      },
-      "log_bucket_ids": {
-        "aws_config": "aws-c2-config-archive",
-        "dns_query_logs": "aws-c2-dns-query-logs-archive",
-        "guardduty": "aws-c2-guardduty-archive",
-        "org_cloudtrail": "aws-c2-cloudtrail-archive",
-        "s3_access_logging": "aws-c2-access-logging",
-        "vpc_flow_logs": "aws-c2-vpc-flow-logs-archive"
-      },
-      "log_bucket_kms_key_arns": {
-        "aws_config": "arn:aws:kms:eu-central-1:872327204802:key/a4b6e73b-e0bd-4987-91cd-fe5ab6632ab1",
-        "dns_query_logs": "arn:aws:kms:eu-central-1:872327204802:key/1e90a551-0e2a-4e5c-885a-eca5e8fd1e77",
-        "guardduty": "arn:aws:kms:eu-central-1:872327204802:key/c06137a0-1273-4d4b-9f0f-88e09aef5166",
-        "org_cloudtrail": "arn:aws:kms:eu-central-1:872327204802:key/7acb498a-ad82-4952-9356-a8f51da82119",
-        "vpc_flow_logs": "arn:aws:kms:eu-central-1:872327204802:key/2783eea5-e29b-49c7-909b-27bcef89f94a"
-      }
-    },
-    "mgmt-organizations": {
-      "global": {
-        "core_regions": [
-          "eu-central-1",
-          "eu-central-2"
-        ],
-        "workload_regions": [
-          "eu-central-1",
-          "eu-central-2"
-        ]
-      },
-      "organization": {
-        "org_id": "o-m29e8d9awz",
-        "org_root_ou_id": "r-6gf5",
-        "ou_ids": {
-          "/root": "r-6gf5",
-          "/root/infrastructure": "ou-6gf5-uktkya48",
-          "/root/sandbox": "ou-6gf5-yvm8rkvb",
-          "/root/security": "ou-6gf5-ebbpq9yb",
-          "/root/suspended": "ou-6gf5-hyl8xkvz",
-          "/root/workloads": "ou-6gf5-6ltp3mjf",
-          "/root/workloads/dev": "ou-6gf5-1mp869fj",
-          "/root/workloads/prod": "ou-6gf5-xrerrg1c",
-          "/root/workloads/test": "ou-6gf5-5ejrrc90"
-        }
-      }
-    },
-    "security": {}
-  }
-  
-  # module.ntc_parameters_reader.all_parameters
+  ntc_parameters = module.ntc_parameters_reader.all_parameters
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -162,33 +71,33 @@ module "ntc_parameters_bucket" {
   }
 }
 
-# # ---------------------------------------------------------------------------------------------------------------------
-# # ¦ NTC PARAMETERS - READER
-# # ---------------------------------------------------------------------------------------------------------------------
-# module "ntc_parameters_reader" {
-#   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/reader?ref=1.1.0"
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ NTC PARAMETERS - READER
+# ---------------------------------------------------------------------------------------------------------------------
+module "ntc_parameters_reader" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/reader?ref=1.1.0"
 
-#   bucket_name = local.ntc_parameters_bucket_name
+  bucket_name = local.ntc_parameters_bucket_name
 
-#   providers = {
-#     aws = aws.euc1
-#   }
-# }
+  providers = {
+    aws = aws.euc1
+  }
+}
 
-# # ---------------------------------------------------------------------------------------------------------------------
-# # ¦ NTC PARAMETERS - WRITER
-# # ---------------------------------------------------------------------------------------------------------------------
-# module "ntc_parameters_writer" {
-#   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/writer?ref=1.1.0"
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ NTC PARAMETERS - WRITER
+# ---------------------------------------------------------------------------------------------------------------------
+module "ntc_parameters_writer" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/writer?ref=1.1.0"
 
-#   bucket_name        = local.ntc_parameters_bucket_name
-#   parameter_node     = local.ntc_parameters_writer_node
-#   node_parameters    = local.ntc_parameters_to_write
-#   store_account_map  = local.ntc_store_account_map
-#   account_map        = local.ntc_account_map
-#   replace_parameters = local.ntc_replace_parameters
+  bucket_name        = local.ntc_parameters_bucket_name
+  parameter_node     = local.ntc_parameters_writer_node
+  node_parameters    = local.ntc_parameters_to_write
+  store_account_map  = local.ntc_store_account_map
+  account_map        = local.ntc_account_map
+  replace_parameters = local.ntc_replace_parameters
 
-#   providers = {
-#     aws = aws.euc1
-#   }
-# }
+  providers = {
+    aws = aws.euc1
+  }
+}
