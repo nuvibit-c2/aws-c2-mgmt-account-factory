cc8f1f5469892f94197781ccbd9dd2f4f6f63e5b
stefano-franco <stefano.franco@nuvibit.com>
2024-08-12 09:01:16 +0200

    fix

--- ntc_account_factory.tf
+++ ntc_account_factory.tf
@@ -243,7 +243,7 @@ module "ntc_account_factory" {
       exclude_accounts_by_tags = []
       # decomissioning of baseline terraform resources must be done before deleting the scope!
       # decommission baseline terraform code for specific accounts in scope
-      decommission_accounts_all         = true
+      decommission_accounts_all         = false
       decommission_accounts_by_ou_paths = []
       decommission_accounts_by_names = [
         # "aws-c2-0002",
