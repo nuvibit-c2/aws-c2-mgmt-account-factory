72ce0d8611fc488840c7fa9a9b3bf37b1a3fb8cb
stefano-franco <stefano.franco@nuvibit.com>
2024-10-22 08:29:51 +0200

    update comment

--- files/account_baseline_poc.tf
+++ files/account_baseline_poc.tf
@@ -70,7 +70,7 @@ module "ntc_r53_records" {
     }
   ]
 
-  # this configuration_alias was dynamically generated with 'baseline_assume_role_providers'
+  # 'aws.connectivity' configuration_alias was dynamically generated with 'baseline_assume_role_providers'
   providers {
     aws = aws.connectivity
   }
