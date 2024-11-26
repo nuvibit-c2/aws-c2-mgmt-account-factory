c59a986ddbf7a7b6efe2d7ab25cbe3048e75e9ea
stefano-franco <stefano.franco@nuvibit.com>
2024-11-26 23:13:39 +0100

    update input

--- ntc_account_factory.tf
+++ ntc_account_factory.tf
@@ -111,21 +111,23 @@ module "ntc_account_factory" {
   # (optional) list of user-defined organizational events to trigger account lifecycle customization step function
   # this can be used to force account lifecycle actions for specified accounts
   # https://docs.aws.amazon.com/organizations/latest/userguide/orgs_cloudtrail-integration.html 
-  account_lifecycle_customization_user_defined_event_triggers = [
-    jsonencode({
-      "source" : "aws.organizations",
-      "detail" : {
-        "eventSource" : "organizations.amazonaws.com",
-        "eventName" : "CreateAccountResult",
-        "serviceEventDetails" : {
-          "createAccountStatus" : {
-            "state": "SUCCEEDED",
-            "accountId" : "228120440352" # aws-c2-management
+  account_lifecycle_customization_on_demand_triggers = {
+    user_defined_events = [
+      jsonencode({
+        "source" : "aws.organizations",
+        "detail" : {
+          "eventSource" : "organizations.amazonaws.com",
+          "eventName" : "CreateAccountResult",
+          "serviceEventDetails" : {
+            "createAccountStatus" : {
+              "state": "SUCCEEDED",
+              "accountId" : "228120440352" # aws-c2-management
+            }
           }
         }
-      }
-    })
-  ]
+      })
+    ]
+  }
 
   # list of baseline definitions for accounts in a specific scope
   account_baseline_scopes = [
