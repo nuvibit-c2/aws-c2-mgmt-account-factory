6059824140b275b9855c619bed59673f5bbee6d5
stefano-franco <stefano.franco@nuvibit.com>
2024-11-27 11:02:05 +0100

    chore: upgrade account factory

--- ntc_account_factory.tf
+++ ntc_account_factory.tf
@@ -28,8 +28,7 @@ locals {
 # ¦ NTC ACCOUNT FACTORY
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_account_factory" {
-  # source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=1.7.0"
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=feat-event-trigger"
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=1.8.0"
 
   # this bucket stores required files for account factory
   account_factory_baseline_bucket_name = "aws-c2-ntc-af-baseline"
