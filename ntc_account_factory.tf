48900b787b383fea525bdc86d4b83e85e27190ad
stefano-franco <stefano.franco@nuvibit.com>
2025-01-21 16:36:53 +0100

    chore: test account factory update

--- ntc_account_factory.tf
+++ ntc_account_factory.tf
@@ -28,7 +28,8 @@ locals {
 # ¦ NTC ACCOUNT FACTORY
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_account_factory" {
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=1.8.2"
+  # source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=1.8.2"
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=fix-harden-s3-bucket-policy"
 
   # this bucket stores required files for account factory
   account_factory_baseline_bucket_name = "aws-c2-ntc-af-baseline"
