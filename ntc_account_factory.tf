3245bcaa1118b95fd53e757f94213ed16702ba9b
burib <burib@users.noreply.github.com>
2025-07-10 09:07:04 +0200

    chore: use terraform-aws-ntc-account-factory@1.10.0
     - switch to python3.13 runtime for lambdas && codebuild

--- ntc_account_factory.tf
+++ ntc_account_factory.tf
@@ -28,7 +28,7 @@ locals {
 # ¦ NTC ACCOUNT FACTORY
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_account_factory" {
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=1.9.0"
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=1.10.0"
 
   # this bucket stores required files for account factory
   account_factory_baseline_bucket_name = "aws-c2-ntc-af-baseline"
