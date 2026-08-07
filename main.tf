0624de3cb96bbfb63795d1a28c47ae91ea8a9c2b
ben-stucke97 <ben.stucke@nuvibit.com>
2026-08-07 10:35:02 +0200

    chore: update terraform version to >=1.6.0 because of migration guide

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 # ¦ REQUIREMENTS
 # ---------------------------------------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
