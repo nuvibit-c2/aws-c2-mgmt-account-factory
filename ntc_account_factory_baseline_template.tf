76014e35338c69cfabe97b2f012ff723a5b7287d
ben-stucke97 <ben.stucke@nuvibit.com>
2026-09-14 08:42:26 +0200

    chore: update to release baseline template version

--- ntc_account_factory_baseline_template.tf
+++ ntc_account_factory_baseline_template.tf
@@ -185,7 +185,7 @@
 #
 # =====================================================================================================================
 module "ntc_account_baseline_templates" {
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-baseline-templates?ref=feature/backup-vault-template"
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-baseline-templates?ref=4.2.0"
 
   # -----------------------------------------------------------------------------------------------------------------
   # ACCOUNT BASELINE TEMPLATES
